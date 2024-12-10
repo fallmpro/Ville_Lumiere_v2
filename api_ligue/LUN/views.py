502bfd92873eea59c67212efcafe7b489ead021c
momof <mouhamad.fall.pro@gmail.com>
2024-12-10 15:51:38 +0100

    Ajout test apl api (fonctionel)

--- api_ligue/LUN/views.py
+++ api_ligue/LUN/views.py
@@ -13,28 +13,41 @@ BASE_URL = 'https://v3.football.api-sports.io/'
 
 # Vue pour la page principale
 # Vue pour la page principale
+# Vue pour la page principale
 def home(request):
-    # URL de l'API pour récupérer les derniers résultats
-    url = f"{BASE_URL}/fixtures"
+    # URL pour récupérer les résultats des matchs
+    url_matches = f"{BASE_URL}/fixtures"
     headers = {
         'x-rapidapi-key': API_KEY,
         'x-rapidapi-host': 'v3.football.api-sports.io'
     }
-    params = {
+    params_matches = {
         'league': 61, 
         'season': 2022 
     }
-    response = requests.get(url, headers=headers, params=params)
-    
-    # Vérification du statut de la réponse
-    if response.status_code == 200:
-        data = response.json()
-        matches = data.get('response', [])  # Les données de match sont dans cette clé
+    response_matches = requests.get(url_matches, headers=headers, params=params_matches)
+
+    if response_matches.status_code == 200:
+        data_matches = response_matches.json()
+        matches = data_matches.get('response', [])
     else:
         matches = []
-    
-    return render(request, 'home.html', {'matches': matches})
 
+    # URL pour récupérer les équipes
+    url_teams = f"{BASE_URL}/teams"
+    params_teams = {
+        'league': 61,
+        'season': 2022
+    }
+    response_teams = requests.get(url_teams, headers=headers, params=params_teams)
+
+    if response_teams.status_code == 200:
+        data_teams = response_teams.json()
+        teams = data_teams.get('response', [])
+    else:
+        teams = []
+
+    return render(request, 'home.html', {'matches': matches, 'teams': teams})
 def teams_view(request):
     teams = Team.objects.all()
     return render(request, 'teams.html', {'teams': teams})
